3601eb9fced7937b23e530f77cf76843f4176d68
undralla naveen <121333046+naveenundralla327@users.noreply.github.com>
2023-11-08 23:16:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const box = document.querySelector('.box');
+
+box.addEventListener('click', () => {
+    container.style.backgroundColor = 'green';
+});
